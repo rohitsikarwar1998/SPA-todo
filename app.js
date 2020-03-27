15adba0551fcd3798a479de4dc9ba52972b59852
ROHIT SIKARWAR <rohitsikarwar1998@gmail.com>
2020-03-27 16:15:25 +0530

    create get and post request with jquery

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require("express"),
-app     = express(),
-mongoose = require("mongoose"),
-bodyParser = require("body-parser"),
-expressSanitizer = require("express-sanitizer"),
-methodOverride = require('method-override');
+  app = express(),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  expressSanitizer = require("express-sanitizer"),
+  methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost/my_database', {
   useMongoClient: true,
 });
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
 app.set("view engine", "ejs");
 app.use(methodOverride('_method'));
@@ -20,70 +20,83 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model("Todo", todoSchema);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.redirect("/todos");
 });
 
-app.get("/todos", function(req, res){
-  Todo.find({}, function(err, todos){
-    if(err){
+app.get("/todos", function (req, res) {
+  Todo.find({}, function (err, todos) {
+    if (err) {
       console.log(err);
     } else {
-      res.render("index", {todos: todos}); 
+      if (req.xhr) res.json(todos);
+      else res.render("index", { todos: todos });
     }
   })
 });
 
-app.get("/todos/new", function(req, res){
- res.render("new"); 
+app.get("/todos/new", function (req, res) {
+  res.render("new");
 });
 
-app.post("/todos", function(req, res){
- req.body.todo.text = req.sanitize(req.body.todo.text);
- var formData = req.body.todo;
- Todo.create(formData, function(err, newTodo){
-    if(err){
+var count = 0;
+
+app.post("/todos", function (req, res) {
+  // console.log(count++);
+  req.body.todo.text = req.sanitize(req.body.todo.text);
+  // console.log(req.body.todo.text);
+  var formData = req.body.todo;
+  Todo.create(formData, function (err, newTodo) {
+    if (err) {
       res.render("new");
     } else {
+      // console.log(req.xhr);
+      if (req.xhr) {
+        res.json(newTodo);
+      }
+      else {
         res.redirect("/todos");
+      }
     }
   });
 });
 
-app.get("/todos/:id/edit", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-     res.redirect("/")
-   } else {
-      res.render("edit", {todo: todo});
-   }
- });
+app.get("/todos/:id/edit", function (req, res) {
+  Todo.findById(req.params.id, function (err, todo) {
+    if (err) {
+      console.log(err);
+      res.redirect("/")
+    } else {
+      res.render("edit", { todo: todo });
+    }
+  });
 });
 
-app.put("/todos/:id", function(req, res){
- Todo.findByIdAndUpdate(req.params.id, req.body.todo, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
-      res.redirect('/');
-   }
- });
+app.put("/todos/:id", function (req, res) {
+  Todo.findByIdAndUpdate(req.params.id, req.body.todo, function (err, todo) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (req.xhr) res.json(todo);
+      else res.redirect('/');
+    }
+  });
 });
 
-app.delete("/todos/:id", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
+app.delete("/todos/:id", function (req, res) {
+  Todo.findById(req.params.id, function (err, todo) {
+    if (err) {
+      console.log(err);
+    } else {
       todo.remove();
-      res.redirect("/todos");
-   }
- }); 
+      if (req.xhr) res.json(todo);
+      else res.redirect("/todos");
+    }
+  });
 });
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Server running on port 3000');
 });
 
--- public/ajax.js
+++ public/ajax.js
@@ -1,1 +1,50 @@
-console.log("every thing is working fine");
+$.get("/todos", data => {
+    console.log(data);
+});
+
+$('form').submit(e => {
+    e.preventDefault();
+    var formData = $('form').serialize();
+    var formAction = $('form').attr('action');
+    // $.post('/todos', formData, function (data) {
+    //     console.log(data);
+    // });
+    // var formAction = $(this).attr('action');
+    $.ajax({
+        type: "POST",
+        url: formAction,
+        data: formData,
+        success: function (response) {
+            console.log(response);
+        },
+        dataType: "json"
+    });
+});
+
+// $('form').submit(e => {
+//     e.preventDefault();
+//     var formData = $('form').serialize();
+//     var formAction = $('form').attr('action');
+//     $.ajax({
+//         type: 'PUT',
+//         url: formAction,
+//         data: formData,
+//         dataType: 'json',
+//         success: function (response) {
+//             console.log(response);
+//         }
+//     });
+// })
+
+
+// $('form').submit(e => {
+//     e.preventDefault();
+//     var formAction = $('form').attr('action');
+//     $.ajax({
+//         url: formAction,
+//         type: 'DELETE',
+//         success: function (response) {
+//             console.log(response);
+//         }
+//     });
+// });
