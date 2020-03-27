b463f87e5e8277a554377dd0b3a3881464516d24
ROHIT SIKARWAR <rohitsikarwar1998@gmail.com>
2020-03-27 16:17:57 +0530

    put and delete request

--- public/ajax.js
+++ public/ajax.js
@@ -2,49 +2,49 @@ $.get("/todos", data => {
     console.log(data);
 });
 
-$('form').submit(e => {
-    e.preventDefault();
-    var formData = $('form').serialize();
-    var formAction = $('form').attr('action');
-    // $.post('/todos', formData, function (data) {
-    //     console.log(data);
-    // });
-    // var formAction = $(this).attr('action');
-    $.ajax({
-        type: "POST",
-        url: formAction,
-        data: formData,
-        success: function (response) {
-            console.log(response);
-        },
-        dataType: "json"
-    });
-});
-
 // $('form').submit(e => {
 //     e.preventDefault();
 //     var formData = $('form').serialize();
 //     var formAction = $('form').attr('action');
+//     // $.post('/todos', formData, function (data) {
+//     //     console.log(data);
+//     // });
+//     // var formAction = $(this).attr('action');
 //     $.ajax({
-//         type: 'PUT',
+//         type: "POST",
 //         url: formAction,
 //         data: formData,
-//         dataType: 'json',
 //         success: function (response) {
 //             console.log(response);
-//         }
-//     });
-// })
-
-
-// $('form').submit(e => {
-//     e.preventDefault();
-//     var formAction = $('form').attr('action');
-//     $.ajax({
-//         url: formAction,
-//         type: 'DELETE',
-//         success: function (response) {
-//             console.log(response);
-//         }
+//         },
+//         dataType: "json"
 //     });
 // });
+
+$('form').submit(e => {
+    e.preventDefault();
+    var formData = $('form').serialize();
+    var formAction = $('form').attr('action');
+    $.ajax({
+        type: 'PUT',
+        url: formAction,
+        data: formData,
+        dataType: 'json',
+        success: function (response) {
+            console.log(response);
+        }
+    });
+})
+
+
+$('form').submit(e => {
+    e.preventDefault();
+    var formAction = $('form').attr('action');
+    $.ajax({
+        url: formAction,
+        type: 'DELETE',
+        success: function (response) {
+            console.log(response);
+        }
+    });
+});
